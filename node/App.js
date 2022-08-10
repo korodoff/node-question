5b8c1f7ddaa6e841c56a1ce4a6c3adec215f1c69
korodoff <rudoffsutnga368@gmail.com>
2022-08-10 12:35:52 +0530

    login validation

--- node/App.js
+++ node/App.js
@@ -4,9 +4,14 @@ const app = express()
 const cors = require("cors")
 const sum = require("./auth/user")
 const {log} = require("./Middleware/middleware")
+const checking = require("./Route/Checking")
 app.use(cors())
 
 app.get("/sum",sum)
+
+
+
+app.use("/auth",checking);
 
 app.get("/",log, function(req,res){
     res.send("hello World")
